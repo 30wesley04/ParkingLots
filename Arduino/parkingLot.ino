15b251bdaf4bb9086406832bd3f74930bc1b4fdc
30wesley04 <wesley3004@hotmail.com>
2023-05-15 08:20:17 -0600

    Update parkingLot.ino

--- Arduino/parkingLot.ino
+++ Arduino/parkingLot.ino
@@ -3,66 +3,54 @@
 #include "FirebaseESP8266.h"
 #include <ArduinoJson.h>
 
+#include <Servo.h>
+
+
+// Host servidor
 #define FIREBASE_HOST "https://luminipark-d68e0-default-rtdb.firebaseio.com"
 #define FIREBASE_AUTH "otJojjEotz31T8Sx7R2wfYDqCUoYMtQcaidjiWzM"
 #define WIFI_SSID "MEGACABLE-2.4G-DFA6"
 #define WIFI_PASSWORD "NYFY4B8JRw"
 
-#define IR_PIN1 D5
-#define IR_PIN2 D6
-#define IR_PIN3 D7
-
 WiFiClient client; 
+Servo servo;
 String firestatus="";
 FirebaseData firebaseData;
 
-int lastOccupiedSpaces = 0;
-
 void setup() {
+  
   Serial.begin(9600);
   WiFi.begin (WIFI_SSID, WIFI_PASSWORD);
-
+    
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
       
-  Serial.println("");
-  Serial.println("Se conectó al wifi!");
+  Serial.println ("");
+  Serial.println ("Se conectó al wifi!");
   Serial.println(WiFi.localIP());
     
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
   Firebase.reconnectWiFi(true);
 
+
+  servo.attach(15); //D1
+  servo.write(0);
   delay(2000);
+
 }
 
 void loop() {
-  Firebase.getInt(firebaseData, "/Estacionamiento/Angelopolis/Secciones/A/spaces");
-  int currentSpaces = firebaseData.intData();
-  
-  int irValue1 = digitalRead(IR_PIN1);
-  int irValue2 = digitalRead(IR_PIN2);
-  int irValue3 = digitalRead(IR_PIN3);
-  
-  int occupiedSpaces = 0;
-  if (irValue1 == LOW) occupiedSpaces++;
-  if (irValue2 == LOW) occupiedSpaces++;
-  if (irValue3 == LOW) occupiedSpaces++;
-  
-  int spacesChanged = occupiedSpaces - lastOccupiedSpaces;
-  if (spacesChanged != 0) {
-    if (currentSpaces >= spacesChanged) {
-      Firebase.setInt(firebaseData, "/Estacionamiento/Angelopolis/Secciones/A/spaces", currentSpaces - spacesChanged);
-      Serial.print("Espacios disponibles: ");
-      Serial.println(currentSpaces - spacesChanged);
-    } else {
-      Serial.println("No hay suficientes espacios disponibles");
-    }
-  } else {
-    Serial.println("No se detectaron cambios");
-  }
 
-  lastOccupiedSpaces = occupiedSpaces;
-  delay(5000);
+  Firebase.getBool(firebaseData,"/Estacionamiento/Angelopolis/Entrada1");
+  bool value=firebaseData.boolData();
+  if(value==true){
+    servo.write(180);
+    delay(9000);
+    servo.write(0);
+    Firebase.setBool(firebaseData,"/Estacionamiento/Angelopolis/Entrada1",false);   
+  }
+  Serial.println (value);
+    
 }
